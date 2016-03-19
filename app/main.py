382aeabdcb6d7621970f939e17dd6939063e9fe6
mzp <mzpppp@gmail.com>
2016-03-19 14:59:14 +0900

    :page_facing_up: add input file page

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, request
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
@@ -6,6 +6,14 @@ app.config.update(DEBUG=True)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/input_files")
+def input_files():
+    return render_template('input_files.html')
+
+@app.route("/import", methods=['POST'])
+def import_files():
+    return "not yet"
 
 @app.route("/fonts/<path:filename>")
 def fonts(filename):
