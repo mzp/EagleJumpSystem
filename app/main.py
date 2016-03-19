931f1428c660527680f610bee6a2bcb0d2fc8ebf
mzp <mzpppp@gmail.com>
2016-03-19 12:28:13 +0900

    :page_facing_up: add HTML templates

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 
 @app.route("/")
 def hello():
-    return "Eagle Jump System"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
