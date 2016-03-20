c61c4103aecafbfca3444c05bb7500bd7d3bfa10
mzp <mzpppp@gmail.com>
2016-03-21 08:58:23 +0900

    :snake: store character data

--- app/db/metadata.py
+++ app/db/metadata.py
@@ -3,7 +3,7 @@ from tinydb import TinyDB, where
 def __db():
     return TinyDB('./data/panels.json')
 
-def update(path, auto_script=None, script=None):
+def update(path, auto_script=None, script=None, characters=None, others=None):
     db = __db()
     if db.contains(where('path') == path):
         data = {}
@@ -11,9 +11,13 @@ def update(path, auto_script=None, script=None):
             data['auto_script'] = auto_script
         if script:
             data['script'] = script
+        if characters:
+            data['characters'] = characters
+        if others:
+            data['others'] = others
         db.update(data, where('path') == path)
     else:
-        db.insert({ 'path': path, 'auto_script': auto_script, script: '' })
+        db.insert({ 'path': path, 'auto_script': auto_script, script: '', 'characters': [], 'others': [] })
 
 def find(path):
     xs = __db().search(where('path') == path)
