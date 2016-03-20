8ea7714615381f29905eebb68183ad3890ec834f
mzp <mzpppp@gmail.com>
2016-03-20 20:03:04 +0900

    :page_facing_up: add face detect page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for, redirect, send_from_directory
 from pages.books import books
+from pages.faces import faces
 from pages.search import search
 from pages.text import text
 from pages.upload import upload
@@ -9,6 +10,7 @@ from pathlib import Path
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 app.register_blueprint(books)
+app.register_blueprint(faces)
 app.register_blueprint(search)
 app.register_blueprint(text)
 app.register_blueprint(upload)
---
+++ app/pages/faces.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, url_for, request, redirect
+import commands
+import commands.faces
+import db.books
+import db.panels
+
+faces = Blueprint('faces', __name__)
+
+@faces.route("/manual/text")
+def index():
+    def count(id, vol):
+        return len(list(db.panels.find(id, vol)))
+    books = db.books.all()
+    return render_template('faces/index.html', books=books, count=count)
+
+@faces.route("/text/<id>/<vol>")
+def detect(id, vol):
+    panels = db.panels.find(id, vol)
+    ident = commands.faces.start(panels)
+    return redirect(url_for('faces.show', id=ident))
+
+@faces.route("/faces/<id>/log")
+def show(id):
+    content = commands.show(id)
+    return render_template('faces/show.html', content=content)
