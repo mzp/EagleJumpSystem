46ffc6f4ec766c265674781434cb93c27e8d36a0
mzp <mzpppp@gmail.com>
2016-03-21 20:42:27 +0900

    :coffee: book select

--- client/containers/Books.js
+++ client/containers/Books.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import actions from '../actions/books';
+import { bindActionCreators } from 'redux';
 
 const template = require('react-jade').compileFile(__dirname + '/Books.jade');
 
 class Books extends React.Component {
   render() {
-    return template({});
+    const { books, select } = this.props;
+    const book = books.find((book) => book.selected);
+    return template({ books, book, select });
   }
 }
 export default connect(
     (state)=> state,
+    (dispatch) => bindActionCreators(actions, dispatch)
   )(Books);
--- client/reducers/books.js
+++ client/reducers/books.js
@@ -1,3 +1,10 @@
 import { handleActions } from 'redux-actions';
 
-export default handleActions({}, []);
+export default handleActions({
+  'books.select': (state, action) => {
+    const id = action.payload;
+    return state.map((book) => {
+      return { ...book, selected: book.id == id };
+    })
+  }
+}, window.__books__ || []);
