e918882b4544bbfd92883ded7a9af2710a17fce9
mzp <mzpppp@gmail.com>
2016-03-23 08:03:14 +0900

    :snake: api-nize /faces

--- app/pages/faces.py
+++ app/pages/faces.py
@@ -8,18 +8,12 @@ faces = Blueprint('faces', __name__)
 
 @faces.route("/faces")
 def index():
-    def count(id, vol):
-        return len(list(db.panels.find(id, vol)))
     books = db.books.all()
-    return render_template('faces/index.html', books=books, count=count)
+    panel_count = db.panels.count_by_books(books)
+    return render_template('faces/index.html', books=books, panel_count=panel_count)
 
 @faces.route("/faces/<id>/<vol>")
 def detect(id, vol):
     panels = db.panels.find(id, vol)
     ident = commands.faces.start(panels)
-    return redirect(url_for('faces.show', id=ident))
-
-@faces.route("/faces/<id>/log")
-def show(id):
-    content = commands.show(id)
-    return render_template('faces/show.html', content=content)
+    return str(ident)
