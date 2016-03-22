fe49a5a06cbcc02d648838c03d4dcc43e42167cd
mzp <mzpppp@gmail.com>
2016-03-22 23:24:22 +0900

    :fire: remove unused page

--- app/pages/upload.py
+++ app/pages/upload.py
@@ -10,19 +10,9 @@ def index():
     books = db.books.all()
     return render_template('upload/index.html', books=books)
 
-@upload.route("/upload/<id>")
-def new(id):
-    book = db.books.find(id)
-    return render_template('upload/new.html', book=book)
-
 @upload.route("/upload/<id>/create", methods=['POST'])
 def create(id):
     volume = request.form['volume']
     images  = request.files.getlist('images')
     ident = commands.cut.start(id, volume, images)
-    return redirect(url_for('upload.show', id=ident))
-
-@upload.route("/upload/<id>/log")
-def show(id):
-    content = commands.show(id)
-    return render_template('upload/show.html', content=content)
+    return str(ident)
