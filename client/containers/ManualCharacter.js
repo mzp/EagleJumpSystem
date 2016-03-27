ca27e147ffc0e89fa597c70e11dc4505e011f31f
mzp <mzpppp@gmail.com>
2016-03-27 19:36:14 +0900

    :coffee: show character info

--- client/containers/ManualCharacter.js
+++ client/containers/ManualCharacter.js
@@ -4,13 +4,30 @@ import { bindActionCreators } from 'redux';
 import volumeActions from '../actions/volume';
 import panelActions from '../actions/panels';
 import VolumeSelect from '../components/VolumeSelect';
+import { nameOfTag } from '../utils';
 
 const template = require('react-jade').compileFile(__dirname + '/ManualCharacter.jade');
 
 class ManualCharacter extends React.Component {
+  selectedPanel() {
+    const { panels } = this.props;
+    return panels.find((panel) => panel.selected);
+  }
+
+  name(tag) {
+    const { books } = this.props;
+    const { book_id } = this.selectedPanel();
+    const book = books.find((book) => book.id == book_id);
+    return nameOfTag(book, tag);
+  }
+
   render() {
+    const panel = this.selectedPanel();
+
     return template({
       VolumeSelect,
+      panel,
+      name: ::this.name,
       ...this.props
     });
   }
