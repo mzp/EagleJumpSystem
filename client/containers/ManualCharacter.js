c374ba2b5977bc7a259bb85f6e94a2ab135781a7
mzp <mzpppp@gmail.com>
2016-03-27 19:49:14 +0900

    :coffee: add input guide

--- client/containers/ManualCharacter.js
+++ client/containers/ManualCharacter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import zip from 'lodash.zip';
 import volumeActions from '../actions/volume';
 import panelActions from '../actions/panels';
 import VolumeSelect from '../components/VolumeSelect';
@@ -9,25 +10,36 @@ import { nameOfTag } from '../utils';
 const template = require('react-jade').compileFile(__dirname + '/ManualCharacter.jade');
 
 class ManualCharacter extends React.Component {
-  selectedPanel() {
+  currentPanel() {
     const { panels } = this.props;
     return panels.find((panel) => panel.selected);
   }
 
-  name(tag) {
+  currentBook() {
     const { books } = this.props;
-    const { book_id } = this.selectedPanel();
-    const book = books.find((book) => book.id == book_id);
+    const { book_id } = this.currentPanel();
+    return books.find((book) => book.id == book_id);
+  }
+
+  name(tag) {
+    const book = this.currentBook();
     return nameOfTag(book, tag);
   }
 
+  guide() {
+    const { characters } = this.currentBook();
+    const keys = ['a', 'o', 'e', 'i', 'u', 'i', 'd', 'h', 't', 'n', 's', '-'];
+    return zip(keys, characters.map((x) => x.name));
+  }
+
   render() {
-    const panel = this.selectedPanel();
+    const panel = this.currentPanel();
 
     return template({
       VolumeSelect,
       panel,
       name: ::this.name,
+      guide: ::this.guide,
       ...this.props
     });
   }
