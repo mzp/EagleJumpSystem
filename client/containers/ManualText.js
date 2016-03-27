47b8aa5a31a520f2e6fffff4ea67f9d6e927ad41
mzp <mzpppp@gmail.com>
2016-03-27 13:55:17 +0900

    :coffee: add hotkeys

--- client/containers/ManualText.js
+++ client/containers/ManualText.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { HotKeys } from 'react-hotkeys';
 import volumeActions from '../actions/volume';
 import panelActions from '../actions/panels';
-import { bindActionCreators } from 'redux';
 import VolumeSelect from '../components/VolumeSelect';
 import ConfirmButton from '../components/ConfirmButton';
 
 const template = require('react-jade').compileFile(__dirname + '/ManualText.jade');
 
 class ManualText extends React.Component {
+  submit(e) {
+    e.preventDefault();
+
+    const {
+      panels,
+      panelAction: { saveScript }
+    } = this.props;
+    const panel = panels.find((panel) => panel.selected);
+    saveScript(panel);
+  }
+
   render() {
     const { panels } = this.props;
     const panel = panels.find((panel) => panel.selected);
-    return template({
+    const keymap = {
+      'submit': 'shift+enter'
+    };
+
+    const handlers = {
+      'submit': ::this.submit
+    };
+
+    const content = template({
       ConfirmButton,
       VolumeSelect,
       panel,
+      submit: ::this.submit,
       ...this.props
     });
+
+    return (<HotKeys keyMap={keymap} handlers={handlers}>{content}</HotKeys>);
   }
 }
 
