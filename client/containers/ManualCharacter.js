ca8b79d6591c27ffd7fdcfba1358017167caae1a
mzp <mzpppp@gmail.com>
2016-03-27 18:31:21 +0900

    :coffee: add side navigation

--- client/containers/ManualCharacter.js
+++ client/containers/ManualCharacter.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import volumeActions from '../actions/volume';
+import panelActions from '../actions/panels';
+import VolumeSelect from '../components/VolumeSelect';
 
 const template = require('react-jade').compileFile(__dirname + '/ManualCharacter.jade');
 
 class ManualCharacter extends React.Component {
   render() {
-    return template({});
+    return template({
+      VolumeSelect,
+      ...this.props
+    });
   }
 }
 
 export default connect(
     (state)=> state,
-    (dispatch) => ({}),
+    (dispatch) => ({
+      volumeAction: bindActionCreators(volumeActions, dispatch),
+      panelAction: bindActionCreators(panelActions, dispatch)
+    }),
   )(ManualCharacter);
