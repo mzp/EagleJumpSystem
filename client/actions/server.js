50ce079fb4f2c2b24bedd2de1668720a2837e178
mzp <mzpppp@gmail.com>
2016-03-31 06:55:00 +0900

    [from now] 2016/03/31 06:55:00

---
+++ client/actions/server.js
@@ -0,0 +1,10 @@
+import { createAction } from 'redux-actions';
+import server from 'gateway/server';
+
+export default {
+  upload: createAction('log.batch', server.upload),
+  textDetect: createAction('log.batch', server.textDetect),
+  faceDetect: createAction('log.batch', server.faceDetect),
+  infer: createAction('log.batch', server.infer),
+  fetch: createAction('log.fetch', server.fetch)
+  }
