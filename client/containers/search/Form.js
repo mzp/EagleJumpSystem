e36703182f08aad9af32631d3277ee0077ca33e9
mzp <mzpppp@gmail.com>
2016-04-02 17:19:27 +0900

    :coffee: add debounce input

--- client/containers/search/Form.js
+++ client/containers/search/Form.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import DebounceInput from 'react-debounce-input';
 import queryAction from 'actions/query';
 import connect from 'containers/supports/connect';
+import { currentBook } from 'reducers/books';
+import get from 'lodash.get';
 
 const template = require('react-jade').compileFile(__dirname + '/Form.jade');
 
@@ -10,8 +13,19 @@ class Form extends React.Component {
     script(e.target.value);
   }
 
+  componentWillUpdate() {
+    const { refs: { query }, props: { query: { script }, books } } = this;
+    const book = currentBook(books);
+
+    if(book.id != get(this.state, 'id', null)) {
+      query.setState({ ...query.state, value: script });
+      this.setState({ id: book.id });
+    }
+  }
+
   render() {
     return template({
+      DebounceInput,
       onChange: ::this.onChange,
       ...this.props
     });
