a98699e4151828d0da8c37e878fd7a22295ffdc2
mzp <mzpppp@gmail.com>
2016-04-02 16:51:07 +0900

    :snake: add /search page

--- app/pages/search.py
+++ app/pages/search.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template
+import db.books
 import db.panels
+import db.faces
+import utils
+import custom_json
 
 search = Blueprint('search', __name__)
 
 @search.route("/")
 def index():
-    panels = db.panels.all()
-    return render_template('search/index.html', panels=panels)
+    books = db.books.all()
+    panels = custom_json.Encoder().encode(db.panels.group_by(books))
+    panel_count = db.panels.count_by_books(books)
+    return render_template('search/index.html', books=books, panels=panels, panel_count=panel_count)
+
+@search.route("/search/<id>")
+def show(id):
+    return index()
