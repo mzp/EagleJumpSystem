c2d619a151d2ee9fd180aa623a2a4082e4250e02
mzp <mzpppp@gmail.com>
2016-04-02 18:29:39 +0900

    :coffee: use react-router

--- client/containers/search/Index.js
+++ client/containers/search/Index.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { browserHistory } from 'react-router'
 import bookAction from 'actions/books';
+import queryAction from 'actions/query';
 import bookSync from 'containers/supports/bookSync';
 import connect from 'containers/supports/connect';
 import BookSelect from 'components/BookSelect';
+import { currentBook } from 'reducers/books';
 
 const template = require('react-jade').compileFile(__dirname + '/Index.jade');
 
 class Index extends React.Component {
+  componentWillMount() {
+    const { queryAction: { script }, location: { query: { q } } } = this.props;
+
+    if(q) {
+      script(path);
+    }
+  }
+
+  componentDidUpdate() {
+    const {
+      books,
+      query: { script },
+      location: { query: { q } }
+    } = this.props;
+
+    const book = currentBook(books);
+
+    if (script && script != q) {
+      browserHistory.push(`/search/${book.id}?q=${script}`);
+    }
+  }
+
   render() {
     const { books } = this.props;
     return template({
@@ -16,4 +41,4 @@ class Index extends React.Component {
   }
 }
 
-export default connect({ bookAction })(bookSync('search', Index));
+export default connect({ bookAction, queryAction })(bookSync('search', Index));
