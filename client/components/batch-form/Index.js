ba98061d5bc47c799c07184fc73753c4d80092f9
mzp <mzpppp@gmail.com>
2016-04-03 10:15:32 +0900

    :coffee: auto scroll at batch form

--- client/components/batch-form/Index.js
+++ client/components/batch-form/Index.js
@@ -4,6 +4,14 @@ import VolumeSelect from 'components/VolumeSelect';
 const template = require('react-jade').compileFile(__dirname + '/Index.jade');
 
 export default class Index extends React.Component {
+  componentWillUpdate() {
+    const main = this.refs.main;
+
+    if(main.scrollTop + main.offsetHeight <= main.scrollHeight) {
+      main.scrollTop = main.scrollHeight - main.offsetHeight;
+    }
+  }
+
   render() {
     return template({
       VolumeSelect,
